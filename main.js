0881433e5d58185645e45dc3f1c56965a929a420
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-10-27 15:48:20 +0100

    Starting

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function (){
+
+// variabili
+
+$('.prev').click( function () {
+    console.log('stai cliccando a sinistra e funziona!');
+
+})
+$('.next').click( function () {
+    console.log('stai cliccando a destra e funziona!');
+    
+})
+
+
+
+
+
+
+
+})
