d2f82320ad0220eed84866affcb4e414624c2d8a
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-10-27 16:18:49 +0100

    Function

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ $(document).ready(function (){
 
 $('.prev').click( function () {
     console.log('stai cliccando a sinistra e funziona!');
-
+    changeImage('.prev');
 })
 $('.next').click( function () {
     console.log('stai cliccando a destra e funziona!');
-    
+    changeImage('.next');
 })
 
 
@@ -18,3 +18,10 @@ $('.next').click( function () {
 
 
 })
+
+// funzioni
+
+function changeImage (parametro) {
+    var actualImage = $('. images img.active');
+    var actualCircle = $('nav i.active');
+}
